d0436db5b7393fd10b89ff2a40e5e12aaa16e673
JonCraig <jonathan.craig@aggiemail.usu.edu>
2021-10-23 17:55:51 -0600

    recursive solution for str builder has been tested, 4 cases made, no DP

--- strBuildr.py
+++ strBuildr.py
@@ -4,17 +4,33 @@
     no spaces will be entered thus striip is not needed
     single line A values only'''
 
-# Practice values
-A = "catinthehat"
-D = ["ca", "tin", "inth", "it", "ehat", "th"]
-# build(A, D) returns true
+# *** Practice values ***
+A0 = "catinthehat"
+D0 = ["ca", "tin", "inth", "it", "ehat", "th"]
+# build(A0, D0) returns true
+A1 = "ttttttttttt"
+D1 = ["g", "a", "t"]
+# returns true
+A2 = "nowheretorunnow"
+D2 = ["this", "will", "return", "false"]
+# return false
+A3 = "ttttttttttttttttttttttttttttttttttty"
+D3 = ["g", "a", "t"]
+# return false
 
+
+# recursive solution
 def build(A, D):
     if len(A) == 0:
         return True
     for i in D:
-        l = len(D[i])
-        if A[:l] == D[i]:
-            if build(A[:l], D):
+        l = len(i)
+        if A[:l] == i:
+            if build(A[l:], D):
                 return True
     return False
+
+As = [A0, A1, A2, A3]
+Ds = [D0, D1, D2, D3]
+for prob in range(4):
+    print(build(As[prob], Ds[prob]))
